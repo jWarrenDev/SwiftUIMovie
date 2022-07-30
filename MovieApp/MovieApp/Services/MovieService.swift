248ed54f7c8f28b7ee7793962a2a0ad4d705344d
Jerrick Warren <Jerrick.Warren@gmail.com>
2022-07-30 16:46:33 -0700

    Finished initial Movie Service

--- MovieApp/MovieApp/Services/MovieService.swift
+++ MovieApp/MovieApp/Services/MovieService.swift
@@ -7,6 +7,29 @@
 
 import Foundation
 
+enum MovieError: Error, CustomNSError {
+    case apiError
+    case invalidEndpoints
+    case invalidRepsonse
+    case noData
+    case serializationError
+    
+    var localizedDescription: String {
+        switch self {
+        case .apiError:
+            return "Error with the Api and fetching Data"
+        case .invalidRepsonse:
+            return "Invalid response"
+        case .invalidEndpoints:
+            return "Invalid Endpoint"
+        case .noData:
+            return "No data was returned"
+        case .serializationError:
+            return "Serilization was not completed properly."
+        }
+    }
+}
+
 enum MovieListEndpoint: String, CaseIterable {
     case nowPlaying = "now_playing"
     case upcoming
@@ -25,4 +48,15 @@ enum MovieListEndpoint: String, CaseIterable {
             return "Popular"
         }
     }
+}
+
+
+protocol MovieService {
+    
+    func fetchMovies(from endpoint: MovieListEndpoint, completion: @escaping (Result<MovieResponse, MovieError>) -> () )
+    
+    func fetchMovie(id: Int , completion: @escaping (Result<Movie, MovieError>) -> () )
+    
+    func searchMovie(query: String, completion: @escaping (Result<MovieResponse, MovieError>) -> () )
+    
 }
