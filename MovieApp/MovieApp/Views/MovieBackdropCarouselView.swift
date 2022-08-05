f55dae2796d9c15ece68d07f9494b20a9beef06a
Jerrick Warren <Jerrick.Warren@gmail.com>
2022-08-04 20:37:31 -0700

    finished Backdrop Carousel

---
+++ MovieApp/MovieApp/Views/MovieBackdropCarouselView.swift
@@ -0,0 +1,40 @@
+//
+//  MovieBackdropCarouselView.swift
+//  MovieApp
+//
+//  Created by Jerrick Warren on 8/4/22.
+//
+
+import SwiftUI
+
+struct MovieBackdropCarouselView: View {
+    
+    let title: String
+    let movies: [Movie]
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0){
+            Text(title)
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.horizontal)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 16) {
+                    ForEach(self.movies) { movie in
+                        MovieBackdropCard(movie: movie)
+                            .frame(width: 272)
+                            .padding(.leading, movie.id == self.movies.first!.id ? 16 : 0)
+                            .padding(.leading, movie.id == self.movies.last!.id ? 16 : 0)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct MovieBackdropCarouselView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieBackdropCarouselView(title: "Latest", movies: Movie.stubbedMovies)
+    }
+}
