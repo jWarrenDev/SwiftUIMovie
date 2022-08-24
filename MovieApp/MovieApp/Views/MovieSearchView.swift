964e9a0cfbd7da833ced4d5b053bdabc344e8d6c
Jerrick Warren <jerrick.warren@gmail.com>
2022-08-24 12:29:06 -0700

    added Searchbar UIViewRepresentatble

---
+++ MovieApp/MovieApp/Views/MovieSearchView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieSearchView.swift
+//  MovieApp
+//
+//  Created by Jerrick Warren on 8/24/22.
+//
+
+import SwiftUI
+
+struct MovieSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieSearchView()
+    }
+}
